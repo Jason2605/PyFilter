729cfdd69367c2b8b07f5eedeb6d4ab9400d4859
Aareon Sullivan <askully13@gmail.com>
2017-11-04 17:36:49 -0700

    Remove too many new lines

--- pyFilter/exceptions.py
+++ pyFilter/exceptions.py
@@ -2,10 +2,6 @@ class ConfigNotFound(Exception):
     """Raises when the config file cant be found"""
     pass
 
-
 class DatabaseConfigException(Exception):
     """Raises when the database supplied in the config does not match sqlite/redis"""
     pass
-
-
-
